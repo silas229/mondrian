aa10920c1b07f387e3f930c888a1f3590ece97ac
Niklas <n.ande@ostfalia.de>
2023-10-12 16:44:44 +0200

    implemented a function to draw a Board, fixed bug in occupiedPositions

--- main.hs
+++ main.hs
@@ -1,3 +1,6 @@
+
+------tests for collisiondetection related functions
+
 testIsColliding :: IO()
 testIsColliding = do -- 2x2 Board, 1x2 Block at (0,0). Tests placing a 1x2 Block at (1,0), a 2x1 Block at (0,1), and a 1x1 Blocks at (0,0), (1,0), (0,1) and (1,1).
     let placedBlock = PlacedBlock { block = Block {blockHeight=2, blockWidth=1, color=Green}, topLeftCorner = Position {x=0, y=0}}
@@ -20,7 +23,7 @@ testIsColliding = do -- 2x2 Board, 1x2 Block at (0,0). Tests placing a 1x2 Block
     let newBlock6 = PlacedBlock { block = Block {blockHeight=1, blockWidth=1, color=Blue}, topLeftCorner = Position {x=1, y=1}}
     let isColliding6 = isColliding board newBlock6
     putStrLn ("Result of testColliding - test 5: " ++ show isColliding6 ++ ", expected: False")
-    
+
 testOccupies :: IO()
 testOccupies = do -- 2x1 Block at (0,1), testing Positions (0,0) and (1,1)
     let placedBlock = PlacedBlock { block = Block {blockHeight=1, blockWidth=2, color=Green}, topLeftCorner = Position {x=0, y=1}}
@@ -46,7 +49,17 @@ testIsOccupied = do -- 2x2 Board with 2x1 Block at (0,1) and 1x1 Block at (0,0),
     let isOccupied3 = isOccupied board position3
     putStrLn ("Result of testIsOccupied - test 3: " ++ show isOccupied3 ++ ", expected: False")
 
+---------tests for Board drawing related functions
 
+--should draw 2x2 Board with 2x1 Block at (0,1) and 1x1 Block at (0,0)
+testDraw :: IO()
+testDraw = do
+    let placedBlock1 = PlacedBlock { block = Block {blockHeight=1, blockWidth=2, color=Green}, topLeftCorner = Position {x=0, y=1}}
+    let placedBlock2 = PlacedBlock { block = Block {blockHeight=1, blockWidth=1, color=Green}, topLeftCorner = Position {x=0, y=0}}
+    let board = Board {boardHeight = 2, boardWidth = 2, placedBlocks = [placedBlock1, placedBlock2]}
+    draw board
+
+--------data type definions
 
 data Color = Red | Green | Blue
 
@@ -73,6 +86,54 @@ data Board = Board
     }
 
 
+------------- functions to draw a Board to the Console
+--draws the Board in the Console. Coordinates occupied by a Block are marked with an X, the rest with an O
+draw :: Board -> IO()
+draw (Board h w blocks) = do
+    let emptyString = replicate (h*w) 'O'
+    let blockedPositions = listOccupiedpositions blocks
+    let blockedPositionInts = map (positionToInt (Board h w blocks)) blockedPositions
+    let filledString = multiReplaceAt "X" emptyString blockedPositionInts
+    let formattedString = multiInsertAt "\n" filledString [ x*w | x <- reverse [1..h-1] ]
+    putStrLn formattedString
+
+positionToInt :: Board -> Position -> Int
+positionToInt (Board h w _) (Position x y) = y * w + x
+
+-- returns all Positions occupied by the PlacedBlocks. May contain duplicates if two PlacedBlocks overlap.
+listOccupiedpositions :: [PlacedBlock] -> [Position]
+listOccupiedpositions placedBlocks = concat (map occupiedPositions placedBlocks)
+
+-- inserts the element into the original list at the supplied index with toInsert - the returned list will therefore contain one more element than original
+insertAt :: [a] -> [a] -> Int -> [a]
+insertAt toInsert original index = do
+    let( start , end ) = splitAt index original
+    start ++ toInsert ++ end
+
+-- uses insertAt to recursively insert toInsert into the original List at all supplied indices
+multiInsertAt :: [a] -> [a] -> [Int] -> [a]
+multiInsertAt toInsert original indices =
+    if indices == []
+        then original
+        else multiInsertAt toInsert (insertAt toInsert original (head indices)) (tail indices)
+
+-- replaces the element in the original list at the supplied index with toInsert
+replaceAt :: [a] -> [a] -> Int -> [a]
+replaceAt toInsert original index = do
+    let( start , _:end ) = splitAt index original
+    start ++ toInsert ++ end
+
+-- uses replaceAt to recursively replace the elements in the original List at all supplied indices with toInsert
+multiReplaceAt :: [a] -> [a] -> [Int] -> [a]
+multiReplaceAt toInsert original indices =
+    if indices == []
+        then original
+        else multiReplaceAt toInsert (replaceAt toInsert original (head indices)) (tail indices)
+
+
+----------functions to check if a PlacedBlock collides with the PlacedBlocks already on a Board
+-- TODO: could probably be simplified by using occupiedPositions and listOccupiedpositions and checking if they contain at least one identical element
+
 -- checks if a new PlacedBlock can be placed on the board, or if it would collide with an already existing PlacedBlock on the Board
 isColliding :: Board -> PlacedBlock -> Bool
 isColliding board newBlock = do -- checks all positions the newBlock occupies
@@ -94,4 +155,4 @@ occupies (Position x y) (PlacedBlock (Block height width _) (Position blockX blo
 
 -- returns a List of all Positions, that a PlacedBlock occupies
 occupiedPositions :: PlacedBlock -> [Position]
-occupiedPositions (PlacedBlock (Block width height _) (Position x y)) = [Position {x=a, y=b} | a <- [x..x+width], b <- [y..y+height]]
+occupiedPositions (PlacedBlock (Block height width _) (Position x y)) = [Position {x=a, y=b} | a <- [x..x+width-1], b <- [y..y+height-1]]
